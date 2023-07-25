538fc820a8bcffafe9804daf53bcdaf7336a5a79
Bruce Hsu <1033684650@qq.com>
2023-07-25 09:39:51 +0800

    initial the tailwindcss

---
+++ AI_Online_PC/postcss.config.cjs
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2023-07-25 09:30:22
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ AI_Online_PC/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2023-07-25 09:30:22
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
