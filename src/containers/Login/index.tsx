06f0b8215fed418b83ee48fb6b4a61da3deaff24
Bruce Hsu <1033684650@qq.com>
2023-07-31 17:57:42 +0800

    finish the login page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-cyan-300 to-blue-300 py-40">
       <div className="container mx-auto">
-        <div className="flex w-8/12 rounded-xl bg-white mx-auto shadow-lg">
+        <div className="flex w-1/2 rounded-xl bg-white mx-auto shadow-lg">
           {/* LEFT */}
           <div className="w-1/3 bg-[url('@/assets/login.jpeg')] text-white bg-contain bg-no-repeat rounded-l-lg flex flex-col item-center justify-center">
             <h1 className="text-3xl font-bold ml-[20%] mb-8">Welcome</h1>
@@ -17,7 +17,7 @@ const Login = () => {
           </div>
           {/* RIGHT */}
           <div className="w-2/3 py-16 px-12">
-            <h2 className="text-3xl mb-4">Register</h2>
+            <h2 className="text-3xl mb-4">登录</h2>
             <p className="mb-4">
               Welcome to this website
             </p>
