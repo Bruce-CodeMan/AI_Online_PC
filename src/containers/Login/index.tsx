d579c93ff44de066ac86fc6cac2a1d7fbb7b96a1
Bruce <1033684650@qq.com>
2023-08-03 00:19:51 +0800

    finish the login page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,20 +1,17 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+
 import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+import axios from "axios"
 
 const Login = () => {
+
+
+  const [ captchaSrc, setCaptchaSrc ] = useState("http://localhost:3000/sms/captcha")
+  
+  const refreshCaptchaHandler = () => {
+    setCaptchaSrc(`http://localhost:3000/sms/captcha?sign=${Math.random()}`)
+  }
+
   return (
     <div className="relative isolate bg-gray-900 min-h-screen">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -97,80 +94,68 @@ const Login = () => {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-white">
-                  First name
+
+            <div>
+                <label className="block text-sm font-semibold leading-6 text-white">
+                  手机号
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
                     className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-white">
-                  Last name
+              </div>
+
+              <div>
+                <label className="block text-sm font-semibold leading-6 text-white">
+                  图片验证码
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
                     className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-              <div className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-white">
-                  Email
+              <div>
+                <div className="mt-9 mr-0">
+                  <img 
+                    src={captchaSrc} 
+                    alt="" 
+                    className='cursor-pointer'
+                    onClick={refreshCaptchaHandler}
+                  />
+                </div>
+              </div>
+              
+              <div>
+                <label className="block text-sm font-semibold leading-6 text-white">
+                  短信验证码
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
+                    type="text"
                     className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-              <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-white">
-                  Phone number
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
-                    autoComplete="tel"
-                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-white">
-                  Message
-                </label>
-                <div className="mt-2.5">
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    defaultValue={''}
-                  />
+              <div>
+                
+                <div className="mt-9 mr-0">
+                  <button className='bg-white/5 py-2 rounded-lg px-6 block text-sm font-semibold leading-6 text-white'>发送短信</button>
                 </div>
               </div>
             </div>
+
+                  
+                  
+
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
