317269466cf7cfb28fc7413e7e4f0441140d78dd
Bruce Hsu <1033684650@qq.com>
2023-08-12 19:22:58 +0800

    add gpt choose model

--- src/containers/ChatGPT/components/NewMessageInput/index.tsx
+++ src/containers/ChatGPT/components/NewMessageInput/index.tsx
@@ -5,15 +5,23 @@
  */
 import { BsSend } from "react-icons/bs";
 import { v4 as uuid } from "uuid"
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useRef } from "react";
 import { useMutation, useQuery } from "@apollo/client";
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 // Custom Imports
 import styles from "./index.module.less"
 import { COMMIT_CONTENT_INFO, GET_CONTENT_INFO } from "@/graphql/content";
 import Message from "../Message";
 import { CHATGPT_URL } from "@/utils/constant";
+
+const people = [
+  { name: 'GPT-3' },
+  { name: 'GPT-4' },
+  { name: 'Claude2' }
+]
 
 interface IProp {
   curKey: string;
@@ -25,6 +33,7 @@ const NewMessageInput = ({ curKey }: IProp) => {
   const [detailContent, setDetailContent] = useState<{ id: string; sender: string; message: string }[]>([]);
   const [isSendMsg, setIsSendMsg] = useState(false)
 
+  const [selected, setSelected] = useState(people[0])
   const [ sendMessage ] = useMutation(COMMIT_CONTENT_INFO)
 
   const {data, refetch} = useQuery(GET_CONTENT_INFO, {
@@ -138,10 +147,61 @@ const NewMessageInput = ({ curKey }: IProp) => {
   return (
     <>
       {/* 模型的选择 -start */}
-      {/* <div className="relative top-0 h-14 w-full rounded-tr-lg border-b-2 flex items-center justify-center">
-        <p className="text-[rgb(217,217,227)] font-bold text-xl">ChatGPT</p>  
+      <div className="relative top-0 h-14 w-full min-h-[60px] rounded-tr-lg border-b-2 flex items-center justify-center">
+      <div className="w-72">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {people.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+      </div>
 
-      </div> */}
+      </div>
       {/* 模型的选择 - end */}
 
       {/* 消息的展示列表 - start */}
@@ -160,7 +220,7 @@ const NewMessageInput = ({ curKey }: IProp) => {
       {/* 消息的展示列表 - end */}
     
       {/* Input 的输入框 - start   */}
-      <div className="relative bottom-0 h-24 w-full rounded-br-lg flex items-center justify-center">
+      <div className="relative bottom-0 h-24 w-full min-h-[96px] rounded-br-lg flex items-center justify-center">
         <input
           placeholder="Send a message"
           value={content}
